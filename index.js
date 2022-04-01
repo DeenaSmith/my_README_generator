1b8051b91bb98060da33e8c14ae1b0d581164ad9
Deena Smith <deenasmith1630@gmail.com>
2022-04-01 12:36:02 -0500

    html template added, input generates responses and sends them to template

--- index.js
+++ index.js
@@ -1,13 +1,52 @@
+
 // TODO: Include packages needed for this application
+const fs = require("fs");
+//const writeToFile = require('./utils/generate.Markdown')
+
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
+
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const readmeDataArgs = process.argv.slice(2, process.argv.length);
+const [name, project] = readmeDataArgs;
+
+ 
+       //HTML template for output
+       const writeToFile = (userName, projectTitle) => {
+        return `
+        <!DOCTYPE html> 
+        <html lang="en"> 
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <meta http-equiv="X-UA-Compatible" content="ie=edge">
+          <title>My README Generator</title>
+        </head>
+      
+        <body>
+          <h1>${name}</h1>
+          <h2>${project}</h2>
+        </body>
+        </html>
+        `;
+    };
+
+
+        //Generates new HTML file after input is complete
+fs.writeFile('index.html', writeToFile(name, project), err => {
+    if (err) throw err;
+
+    console.log('README generated! Check out index.html to see the output!');
+});
+
+
+
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
@@ -16,19 +55,6 @@ init();
 
 
 
-const readmeDataArgs = process.argv.slice(2, process.argv.length);
-console.log(readmeDataArgs);
 
 
-
-const printReadmeData = readmeDataArgs => {
-    for (let i = 0; i < readmeDataArgs.length; i += 1) {
-        console.log(readmeDataArgs[i]);
-    }
-
-    console.log('==========');
-
-    readmeDataArgs.forEach(readmeItem => console.log(readmeItem))
-    };
-;
 
