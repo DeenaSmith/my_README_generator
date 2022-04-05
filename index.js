a60b5098b4f9c332c59fbfac063a4479ebd2ac3f
Deena Smith <deenasmith1630@gmail.com>
2022-04-05 17:13:46 -0500

    questions prompt when file is run, html deleted from project

--- index.js
+++ index.js
@@ -2,10 +2,71 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
 const fileUtils = require('./utils/generate.Markdown')
+const inquirer = require("inquirer");
 
 
 // TODO: Create an array of questions for user input
 const questions = [];
+
+inquirer.prompt(
+  [
+    {
+      type: 'input',
+      message: "What is the title of your project?",
+      name: 'title',
+      validate: (value) => { if (value){return true} else {return 'You must enter a value to proceed.'}},
+    },
+    {
+      type: 'input',
+      message: "Please describe your project.",
+      name: 'description',
+      validate: (value) => { if (value){return true} else {return 'You must enter a value to proceed.'}},
+    },
+    {
+      type: 'input',
+      message: "Are there any installation instructions for your project?",
+      name: 'installation',
+      validate: (value) => { if (value){return true} else {return 'You must enter a value to proceed.'}},
+    },
+    {
+      type: 'input',
+      message: "Is there any spectific usage information?",
+      name: 'usage',
+      validate: (value) => { if (value){return true} else {return 'You must enter a value to proceed.'}},
+    },
+    {
+      type: 'input',
+      message: "What are the contribution guidelines for your project?",
+      name: 'contribution',
+      validate: (value) => { if (value){return true} else {return 'You must enter a value to proceed.'}},
+    },
+    {
+      type: 'input',
+      message: "Are there any testing instructions?",
+      name: 'testing',
+      validate: (value) => { if (value){return true} else {return 'You must enter a value to proceed.'}},
+    },
+    {
+      type: 'list',
+      message: "What license is your project under?",
+      name: 'license',
+      choices: ['Apache', 'MIT', 'GPL', 'ISC', 'NA'],
+      validate: (value) => { if (value){return true} else {return 'You must enter a value to proceed.'}},
+    },
+    {
+      type: 'input',
+      message: "What is your GitHub username?",
+      name: 'username',
+      validate: (value) => { if (value){return true} else {return 'You must enter a value to proceed.'}},
+    },
+    {
+      type: 'input',
+      message: "What is your email address?",
+      name: 'email',
+      validate: (value) => { if (value){return true} else {return 'You must enter a value to proceed.'}},
+    }
+  ]
+)
 
 
 
@@ -19,29 +80,16 @@ fileUtils.generateMarkdown( {
        //HTML template for output
        const writeToFile = (userName, projectTitle) => {
         return `
-        <!DOCTYPE html> 
-        <html lang="en"> 
-        <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <meta http-equiv="X-UA-Compatible" content="ie=edge">
-          <title>My README Generator</title>
-        </head>
-      
-        <body>
-          <h1>${name}</h1>
-          <h2>${project}</h2>
-        </body>
-        </html>
+        
         `;
     };
 
 
         //Generates new HTML file after input is complete
-fs.writeFile('index.html', writeToFile(name, project), err => {
+fs.writeFile('README.md', writeToFile(name, project), err => {
     if (err) throw err;
 
-    console.log('README generated! Check out index.html to see the output!');
+   // console.log('README generated! Check out README.md to see the output!');
 });
 
 
